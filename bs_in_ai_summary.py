bd0d53e7a2bae6ce20f3f05296fae8801e580271
Jordan Boyd-Graber <jbg@boydgraber.org>
2024-11-23 16:45:44 -0500

    Need to add number of repeated courses, remove ROOT

--- bs_in_ai_summary.py
+++ bs_in_ai_summary.py
@@ -6,7 +6,7 @@ from collections import defaultdict
 from typing import Dict, Union, Iterable
 import graphviz
 
-def extract_dependencies_and_sort(graph: Dict[str, Union[str, Iterable[str]]]) -> Dict[str, Iterable[str]]:
+def extract_dependencies_and_sort(graph: Dict[str, Union[str, Iterable[str]]]) -> Iterable[Iterable[str]]:
     temp_graph = {}
 
     for ii in graph:
@@ -16,8 +16,16 @@ def extract_dependencies_and_sort(graph: Dict[str, Union[str, Iterable[str]]]) -
     result[0] = set(['ROOT'])
     return result
 
-def create_graphviz(meta: Dict[str, Iterable[str]], name='', output=''):
+def create_graphviz(meta: Dict[str, Iterable[str]], name='', output='', concentrations=['core']):
     dot = graphviz.Digraph(comment=name)
+
+    needed_nodes = []
+    for node in meta:
+        if meta[node]["Concentration"] in concentrations:
+            needed_nodes.append(node)
+
+        for requirements in meta[node]['Dependencies']:
+            needed_nodes.append(requirements)
 
     for node in meta:
         label = meta[node]['Pretty Label']
@@ -30,12 +38,70 @@ def create_graphviz(meta: Dict[str, Iterable[str]], name='', output=''):
 
     print(dot.source)
     dot.render(output)
-                           
 
-def dependency_graph_from_df(courses_df: DataFrame) -> Dict[str, Union[str, Iterable[str]]]:
+
+def gather_prerequisites(all_requirements, concept):
+    if concept == "ROOT":
+        return set()
+    else:
+        result = set()
+        for depdendency in all_requirements[concept]['Dependencies']:
+            if depdendency != "ROOT":
+                result.add(depdendency)
+                result = result | gather_prerequisites(all_requirements, depdendency)
+        return result
+
+def compute_priorities(requirements):
+
+    priority = defaultdict(int)
+
+    for course in requirements:
+        for prereq in requirements[course]:
+            priority[prereq] += 1
+
+    return priority
+
+def create_schedule(topological_sort, prerequisites, priorities, course_descriptions, courses_per_time=3):
+
+    non_prioritized = []
+    for equivalence_set in topological_sort:
+        non_prioritized += equivalence_set
+
+    courses = defaultdict(list)
+    added = set()
+
+    time = 1
+    for time in range(8):
+        while len(courses[time]) < courses_per_time:
+            candidates = [x for x in non_prioritized if x not in added]
+            print(time, candidates)
+
+            if len(candidates) == 0:
+                break
+
+            valid_courses = [x for x in priorities if x in candidates and set(prerequisites[x]) - added == set()]
+
+            if False:
+                max_priority = max(priorities[x] for x in valid_courses)
+            
+                course_to_add = [x for x in priorities if priorities[x] == max_priority][0]
+            else:
+                
+                course_to_add = candidates[0]
+
+            courses[time] += [course_to_add]
+            added.add(course_to_add)
+            
+        courses[time]
+
+    return courses
+
+
+
+
+
+def dependency_graph_from_df(courses_df: DataFrame, graph: Dict[str, Dict[str, Iterable[str]]] = defaultdict(dict)) -> Dict[str, Dict[str, Iterable[str]]]:
     all_nodes = set(courses_df["ID"])
-
-    graph = defaultdict(defaultdict)
 
     for values in [values for row, values in courses_df.iterrows() if values["ID"]]:
         id = values["ID"]
@@ -58,10 +124,23 @@ def dependency_graph_from_df(courses_df: DataFrame) -> Dict[str, Union[str, Iter
     return graph
 
 if __name__ == "__main__":
-    for file in ['core']:
-        with open("%s.csv" % file) as infile:
-            raw = pandas.read_csv(infile)
-            meta = dependency_graph_from_df(raw)
-            topo = extract_dependencies_and_sort(meta)
-            create_graphviz(meta, output=file)
+    file = "core"
+    with open("course_source/%s.csv" % file) as infile:
+        raw = pandas.read_csv(infile)
+        meta = dependency_graph_from_df(raw)
+        topo = extract_dependencies_and_sort(meta)
+
+        prerequisites = {}
+        for ii in meta:
+            prerequisites[ii] = gather_prerequisites(meta, ii)
+
+        print("Prerequisites", prerequisites)
+        priorities = compute_priorities(prerequisites)
+
+        print("Priorities", priorities)
+
+        schedule = create_schedule(topo, prerequisites, priorities, meta)
+        print("Schedule", schedule)
+
+        create_graphviz(meta, output=file)
     
